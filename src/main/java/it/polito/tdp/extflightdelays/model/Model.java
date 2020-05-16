05862b82403af2903c3a5ec43505545ea71adca3
sonia <sonia@DESKTOP-BGUL1JV>
2020-05-16 10:57:50 +0200

    commit

--- src/main/java/it/polito/tdp/extflightdelays/model/Model.java
+++ src/main/java/it/polito/tdp/extflightdelays/model/Model.java
@@ -42,22 +42,21 @@ public class Model {
 				//inserisco aeroporto come vertice
 				this.grafo.addVertex(a);
 			}
-
-			for(Rotta r : dao.getRotte(idMap)) {
+		}
+		
+		for(Rotta r : dao.getRotte(idMap)) {
+			
+			if(this.grafo.containsVertex(r.getA1()) && this.grafo.containsVertex(r.getA2())) {
 				
-				//if(this.grafo.containsVertex(r.getA1()) && this.grafo.containsVertex(r.getA2())) {
+				DefaultWeightedEdge e = this.grafo.getEdge(r.a1, r.a2);
+				if(e == null) {
+					Graphs.addEdgeWithVertices(this.grafo, r.getA1(), r.getA2(), r.getPeso());
+				} else {
+					double pesoVecchio = this.grafo.getEdgeWeight(e);
+					double pesoNuovo = pesoVecchio + r.getPeso();
+					this.grafo.setEdgeWeight(e, pesoNuovo);
 					
-					DefaultWeightedEdge e = this.grafo.getEdge(r.a1, r.a2);
-					if(e == null) {
-						Graphs.addEdgeWithVertices(this.grafo, r.getA1(), r.getA2(), r.getPeso());
-					} else {
-						double pesoVecchio = this.grafo.getEdgeWeight(e);
-						double pesoNuovo = pesoVecchio + r.getPeso();
-						this.grafo.setEdgeWeight(e, pesoNuovo);
-						
-					}
-				//}
-				
+				}
 			}
 		}
 		
@@ -129,7 +128,7 @@ public class Model {
 		
 		
 		while(it.hasNext()) {
-			it.hasNext();
+			it.next();
 		}
 		
 		
